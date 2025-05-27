b9ac34afbc3936e4c74c204499bab9ff5fd0767e
ikram <ikramelazhary78@gmail.com>
2025-05-27 01:01:56 +0100

    second commit

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-
-
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 //app config
 const app = express()
@@ -21,6 +22,9 @@ connectDB();
 
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
