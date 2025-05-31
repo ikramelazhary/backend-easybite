57a4ce43935972b3c394d936da1f852798b677bd
ikram <ikramelazhary78@gmail.com>
2025-05-31 17:12:01 +0100

    djdjsfd

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ export const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
